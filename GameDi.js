b5d92ad90706fbfba5e5888d5940c92e8dac06c9
Jehyeop-Yu <wpguq96@naver.com>
2021-03-21 16:43:08 +0900

    gaimedi_logo_page

---
+++ GameDi.js
@@ -0,0 +1,5 @@
+var startClick = function(){
+    document.querySelector('div.a').style.display = "none";
+    document.querySelector('div.b').style.display = "block";
+    setTimeout(function(){location.href="https://www.naver.com"},300);
+}
